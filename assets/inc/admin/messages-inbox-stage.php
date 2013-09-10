c096980ea4a5d9120c0d450ca927d0f291884ded
marinonata <natashamarino@Natashas-MacBook-Pro.local>
2013-09-10 12:28:12 -0400

    messages page

--- assets/inc/admin/messages-inbox-stage.php
+++ assets/inc/admin/messages-inbox-stage.php
@@ -13,6 +13,14 @@
 				<button type="button" class="btn btn-default btn-sm">
 					Archive
 				</button>
+				<button type="button" class="btn btn-default btn-sm">
+					Read
+				</button>
+				<button type="button" class="btn btn-default btn-sm">
+					Unread
+				</button>
+
+				<p class="sort_section">Sort: <a href="#">newest</a> | <a href="#">oldest</a> </p>
 			</div>
 			<div class="admin_stage_content">
 				
@@ -20,6 +28,44 @@
 					<div class="messages_inbox_controls">
 					</div>
 					<div class="messages_inbox_messages">
+						<div class="messages_message">
+							<div class="col-md-7">
+								<input class="pull-left" type="checkbox" name="message_select" />
+								<img class="img-circle pull-left message_avatar" src="<?=ASSETS;?>img/temp_avatar.jpg">
+								<div class="message_details pull-left">
+									<span class="message_from">John Smith</span>
+									<span class="message_subject">This is the subject line of a new message</span>
+								</div>
+							</div>
+							<div class="col-md-5">
+								<span class="message_date">July 1</span>
+								<ul class="message_controls list-inline">
+									<li><i class="icon-mail-reply"></i> reply</li>
+									<li><i class="icon-mail-forward"></i> forward</li>
+									<li><i class="icon-remove"></i> delete</li>
+								</ul>
+							</div>
+						</div>
+
+						<div class="messages_message">
+							<div class="col-md-7">
+								<input class="pull-left" type="checkbox" name="message_select" />
+								<img class="img-circle pull-left message_avatar" src="<?=ASSETS;?>img/temp_avatar.jpg">
+								<div class="message_details pull-left">
+									<span class="message_from">John Smith</span>
+									<span class="message_subject message_read">This is the subject line of a read message</span>
+								</div>
+							</div>
+							<div class="col-md-5">
+								<span class="message_date">July 1</span>
+								<ul class="message_controls list-inline">
+									<li><i class="icon-mail-reply"></i> reply</li>
+									<li><i class="icon-mail-forward"></i> forward</li>
+									<li><i class="icon-remove"></i> delete</li>
+								</ul>
+							</div>
+						</div>
+
 						<div class="messages_message">
 							<div class="col-md-7">
 								<input class="pull-left" type="checkbox" name="message_select" />
@@ -38,6 +84,7 @@
 								</ul>
 							</div>
 						</div>
+
 					</div>
 				</div>
 			</div>
