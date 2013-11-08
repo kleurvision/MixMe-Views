dd82b64b2fcfc5c7ad8a82285856722387e6fafe
Patrick Lyver <patrick@kleurvision.com>
2013-11-08 18:28:31 -0500

    Updates

--- public/band-profile/inc/activity_meta.php
+++ public/band-profile/inc/activity_meta.php
@@ -4,16 +4,39 @@
 	<a href="#"><i class="icon-star activity-icons pull-right"></i></a><a href="#"><i class="icon-share activity-icons pull-right activity-icons-share"></i></a>
 </div>
 <div class="row">
-	<div id="reply<?=$r;?>" class="collapse reply_stage">
-		<div class="row">
+	<div class="replies">
+		
+		<div class="row user-reply">
 			<div class="col-md-1 action_activity_left">
 				<div class="global_userAvatar">
 					<a><img src="<?=ASSETS;?>/img/temp_avatar.jpg"></a>
 				</div>
 			</div>
 			<div class="col-md-11 action_activity_right">
-				Reply
+				Looks amazing!
 			</div>
+		</div>
+		
+		<div class="row user-reply">
+			<div class="col-md-1 action_activity_left">
+				<div class="global_userAvatar">
+					<a><img src="<?=ASSETS;?>/img/temp_avatar.jpg"></a>
+				</div>
+			</div>
+			<div class="col-md-11 action_activity_right">
+				Thanks for sharing Elaine...
+			</div>
+		</div>
+		
+		
+	</div>
+</div>
+<div class="row">
+	<div id="reply<?=$r;?>" class="collapse reply_stage">
+		<div class="row">
+			<form role="form">
+				<input class="form-control input-lg" placeholder="Reply to message..."/>
+			</form>
 		</div>
 	</div>
 </div>
