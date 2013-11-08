ff6c325569a4a2b2da2b87ea81915bc9c19b0c09
Patrick Lyver <patrick@kleurvision.com>
2013-11-08 18:00:34 -0500

    Updates

--- public/band-profile/inc/activity_meta.php
+++ public/band-profile/inc/activity_meta.php
@@ -1,2 +1,11 @@
-<p class="action_activity_meta pull-left">an hour ago&nbsp;&middot;&nbsp;<a href="">reply</a></p> 
-<a href="#"><i class="icon-star activity-icons pull-right"></i></a><a href="#"><i class="icon-share activity-icons pull-right activity-icons-share"></i></a>
+<? $r = 1;?>
+<div class="row">
+	<p class="action_activity_meta pull-left">an hour ago&nbsp;&middot;&nbsp;<a data-toggle="collapse" data-target="#reply<?=$r;?>">reply</a></p> 
+	<a href="#"><i class="icon-star activity-icons pull-right"></i></a><a href="#"><i class="icon-share activity-icons pull-right activity-icons-share"></i></a>
+</div>
+<div class="row">
+	<div id="reply<?=$r;?>" class="collapse reply_stage">
+		Reply
+	</div>
+</div>
+<? $r++;?>
--- public/band-profile/inc/band-top.php
+++ public/band-profile/inc/band-top.php
@@ -4,13 +4,53 @@
 	</h1>
 	<div class="profile_controls pull-right">
 		<div class="btn-group">
-			<button type="button" class="btn btn-default"><i class="icon-dollar"></i></button>
+			<button type="button" role="button" class="btn btn-default" href="#tipModal" data-toggle="modal"><i class="icon-dollar"></i></button>
 			<button type="button" class="btn btn-default"><i class="icon-ok"></i></button>
 			<button type="button" class="btn btn-default"><i class="icon-thumbs-up"></i></button>
 			<button type="button" class="btn btn-default"><i class="icon-thumbs-down"></i></button>
 		</div>
-		<div class="btn profile_broadcasting global_action_btn gradient">
+		<div class="btn profile_broadcasting gradient">
 		Online
+		</div>
+	</div>
+</div>
+
+
+<!-- Tip Modal -->
+<div id="tipModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="tipModalLabel" aria-hidden="true" style="display:none;">
+	<div class="modal-dialog">
+		<div class="modal-content">
+		<form class="form-horizontal">
+			<div class="modal-header admin_stage_head">
+			    	<h2 class="admin_stage_title stage_title">Tip Artist</h2>
+			    	<button type="button" class="close" aria-hidden="true">&times;</button>
+			</div>
+			<div class="modal-body fan_target_content">
+				<div class="t_modal_content">
+					<div class="form-group">
+						<label class="modal_form_label_tip control-label col-md-4">Your Account Balance:</label>
+						<div class="col-md-5">
+							<input class="form-control" value="23.95" type="text" disabled></input>
+						</div>
+						<div class="col-md-3">
+							<button class="btn btn-primary pull-right"><i class="icon-dollar"></i> Add Funds</button>
+						</div>
+					</div>
+					<hr/>
+					<div class="form-group">
+						<label class="modal_form_label_tip control-label col-md-4">Tip Amount:</label>
+						<div class="col-md-8">
+							<input class="form-control" placeholder="0.00" type="text"></input>
+						</div>
+					</div>
+		 			
+				</div>
+			</div>
+			<div class="admin_stage_bottom">
+					<button class="btn modal-save-cancel global_action_btn gradient pull-right">Send Tip!</button>
+					<button class="btn modal-save-cancel global_action_btn gradient pull-right" data-dismiss="modal" aria-hidden="true">Cancel</button>
+			</div>
+			</form>
 		</div>
 	</div>
 </div>
